1cf33c021935e2933b50b097cc057bbcd9061464
陈科研 <chenkeyan@xiaohongshu.com>
2023-12-30 17:38:04 +0800

    add

--- README.md
+++ README.md
@@ -1,9 +1,14 @@
 ---
-title: TTP Demo Space
-emoji: 🤗
-colorFrom: yellow
-colorTo: orange
+title: TTP
+emoji: 📉
+colorFrom: red
+colorTo: yellow
 sdk: gradio
+sdk_version: 4.12.0
 app_file: app.py
 pinned: false
+license: apache-2.0
 ---
+
+This is the demo of ["Time Travelling Pixels: Bitemporal Features Integration with Foundation Model for Remote Sensing Image Change Detection"](https://arxiv.org/abs/2312.16202). Seeing [Github](https://github.com/KyanChen/TTP) for more information!
+
--- app.py
+++ app.py
@@ -25,6 +25,13 @@ def infer(img1, img2):
 
 
 with gr.Blocks() as demo:
+    gr.Markdown(
+        """
+        # [Time Travelling Pixels: Bitemporal Features Integration with Foundation Model for Remote Sensing Image Change Detection](https://arxiv.org/abs/2312.16202)
+        """)
+    # a empty row
+    gr.Row()
+
     with gr.Row():
         input_0 = gr.Image(label='Input Image1')
         input_1 = gr.Image(label='Input Image2')
@@ -36,7 +43,13 @@ with gr.Blocks() as demo:
     img1_files = glob.glob('samples/A/*.png')
     img2_files = [f.replace('A', 'B') for f in img1_files]
     input_files = [[x, y] for x, y in zip(img1_files, img2_files)]
-    gr.Examples(input_files, fn=infer, inputs=[input_0, input_1], outputs=[output_gt], cache_examples=True)
+    gr.Examples(input_files, fn=infer, inputs=[input_0, input_1], outputs=[output_gt], cache_examples=False)
+    gr.Markdown(
+        """
+			This is the demo of ["Time Travelling Pixels: Bitemporal Features Integration with Foundation Model for Remote Sensing Image Change Detection"](https://arxiv.org/abs/2312.16202). Seeing [Github](https://github.com/KyanChen/TTP) for more information!
+		""")
+    # a empty row
+    gr.Row()
 
 if __name__ == "__main__":
     demo.launch()
